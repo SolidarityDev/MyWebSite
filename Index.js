fcff3d2b6f4ee36b45c3bede61eb9d650ff5d0da
Seraphin <aribi.rachid@hotmail.fr>
2020-07-18 21:00:44 +0200

    first commit

---
+++ Index.js
@@ -0,0 +1,11 @@
+<script>
+ document.addEventListener ('DOMContentLoaded', function(){  
+const btnSubmit = document.getElementById('btnsubmit');
+btnSubmit.addEventListener('click, alerte);');
+
+    function alerte(){
+        alert('Merci pour votre message !');
+    }
+});
+
+</script>
